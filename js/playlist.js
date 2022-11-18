56ae2c96d15cfafa3a84ee1b61a01ca2e10e4a35
Bruno Dorea <brunodorea@outlook.com.br>
2022-11-18 15:59:08 -0300

    update

--- js/playlist.js
+++ js/playlist.js
@@ -12,33 +12,38 @@ function Sample(name, file, key, volume = 1, isBgm = false) {
   }
   
 export let playlist = [
-  //
-    new Sample("", "", "KeyQ"),
-    new Sample("", "", "KeyW"),
-    new Sample("", "", "KeyE"),
-    new Sample("", "", "KeyR"),
-    new Sample("", "", "KeyT"),
-    new Sample("", "", "KeyY"),
-    new Sample("", "", "KeyU"),
-    new Sample("", "", "KeyI"),
+    // 1ª LINHA DE TECLAS
+    // new Sample("", "", "KeyQ"),
+    // new Sample("", "", "KeyW"),
+    // new Sample("", "", "KeyE"),
+    // new Sample("", "", "KeyR"),
+    // new Sample("", "", "KeyT"),
+    // new Sample("", "", "KeyY"),
+    new Sample("Um beijo no seu coração", "um-beijo", "KeyU"),
+    // new Sample("", "", "KeyI"),
     new Sample("O caaaaaaaaaaos!", "o-caaaaaaaaaos", "KeyO"),
-    new Sample("", "", "KeyP"),
-    //
-    new Sample("", "", "KeyA"),
+    // new Sample("Onde está o seu Deus?", "onde-esta-o-seu-deus", "KeyO"),
+    // new Sample("", "", "KeyP"),
+
+    // 2ª LINHA DE TECLAS
+    new Sample("Aqui Não! Sua Lambisgoia!", "aqui-nao", "KeyA"),
+    // new Sample("Aqui Não! Sua Sirigaita!", "aqui-nao2", "KeyA"),
+    // new Sample("Assererrê !", "assererre", "KeyA"),
     new Sample("Senhora! Volta aqui senhora!", "senhora-volta-aqui-senhora", "KeyS"),
-    new Sample("", "", "KeyD"),
-    new Sample("", "", "KeyF", 2),
-    new Sample("", "", "KeyG", 2),
-    new Sample("", "", "KeyH", 2),
-    new Sample("", "", "KeyJ", 2),
-    new Sample("", "", "KeyK", 2),
-    new Sample("", "", "KeyL", 2),
-    //
-    new Sample("", "", "KeyZ", 0.5, true),
-    new Sample("", "", "KeyX", 1, true),
-    new Sample("", "", "KeyC", 0.9, true),
-    new Sample("", "", "KeyV", 1, true),
-    new Sample("", "", "KeyB", 1, true),
-    new Sample("", "", "KeyN", 1, true),
-    new Sample("", "", "KeyM", 1, true),
+    // new Sample("", "", "KeyD"),
+    // new Sample("", "", "KeyF", 2),
+    // new Sample("", "", "KeyG", 2),
+    // new Sample("", "", "KeyH", 2),
+    // new Sample("", "", "KeyJ", 2),
+    // new Sample("", "", "KeyK", 2),
+    // new Sample("", "", "KeyL", 2),
+
+    // 3ª LINHA DE TECLAS
+    // new Sample("", "", "KeyZ", 0.5, true),
+    // new Sample("", "", "KeyX", 1, true),
+    // new Sample("", "", "KeyC", 0.9, true),
+    // new Sample("", "", "KeyV", 1, true),
+    new Sample("Boom Boom Boom Shakalaka", "boom-shakalaka", "KeyB", 1, true),
+    // new Sample("", "", "KeyN", 1, true),
+    // new Sample("", "", "KeyM", 1, true),
   ];
